64af1c96e9bc7ed565bfdf3092d3818a4cc1e65f
Priyansh Dimri <priyanshd0908@gmail.com>
2024-03-05 12:53:19 +0530

    Added necessary files for setting up starting point for development environment

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+// routes/index.js
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('Hello, World!');
+});
+
+module.exports = router;
