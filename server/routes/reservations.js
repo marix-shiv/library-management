56e18381476a7813b569edaed4829db7522cc35b
Priyansh Dimri <priyanshd0908@gmail.com>
2024-03-17 14:50:01 +0530

    Added Top Level comments in various files

--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -1,3 +1,16 @@
+/**
+ * This module sets up all routes for the reservations resource.
+ * 
+ * The routes include:
+ * - GET /: Get all reservations
+ * - GET /user/:id: Get all reservations made by a specific user
+ * - GET /book/:id: Get all reservations for a specific book
+ * - GET /:id: Get a specific reservation by its ID
+ * - POST /: Create a new reservation
+ * - DELETE /:id: Delete a specific reservation by its ID
+ * 
+ * Each route is linked to a function in the reservations controller.
+ */
 const express = require('express');
 const router = express.Router();
 
